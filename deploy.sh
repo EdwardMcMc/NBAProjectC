c95572a5b0bff4625ea8a8464a51d2fc82d389b7
EDWARD MCKENZIE-MCHARG <102103545@student.swin.edu.au>
2019-05-22 05:56:18 +0000

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ npm run build
 # Add these to a new git commit
 # Push
 git clone $DESTINATION_REPO
-cp -r build/* nbaprojectc.github.io/NBAProjectCProduction
-cd nbaprojectc.github.io/NBAProjectCProduction
+cp -r build/* nbaprojectc.github.io/
+cd nbaprojectc.github.io
 git add .
 git commit -m 'site update'
 git push origin master
