1ac5f2497064d0683ae351eacec186cccb6dc4e4
EDWARD MCKENZIE-MCHARG <102103545@student.swin.edu.au>
2019-05-22 02:05:57 +0000

    Added file deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Git config
+git config --global user.email "nbaprojectc@nxk.io"
+git config --global user.name "NBAProjectC"
+
+# Build
+npm run build
+
+# Add these files to the github pages repo overwriting what currently exists
+# Add these to a new git commit
+# Push
+git clone $DESTINATION_REPO
+cp -r build/* nbaprojectc.github.io/NBAProjectCProduction
+cd nbaprojectc.github.io/NBAProjectCProduction
+git add .
+git commit -m 'site update'
+git push origin master
+
